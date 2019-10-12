790e46cf3df61d559da2b4417d854712bb7d72af
astair <jonas.simon.fleck@gmail.com>
2019-10-12 23:17:19 +0200

    structure_markers now takes stage names instead of matrix.

--- R/differential_expression.R
+++ R/differential_expression.R
@@ -2,22 +2,34 @@
 #'
 #' @export
 #'
-structure_markers <- function(expr_mat,
-                                   annotation_level = NULL,
-                                   structure_groups = NULL
+structure_markers <- function(
+    stage = 'E13pt5',
+    annotation_level = 'custom_3',
+    structure_groups = NULL
 ){
     utils::data(voxel_meta, envir = environment())
 
-    if (is.null(annotation_level)){
+    if (!exists('DATA_LIST') | !exists('PATH_LIST')){
+        stop('Data has not been loaded. Please run load_aba_data() first.')
+    }
+
+    stage <- stage_name(stage)
+    if (is.null(DATA_LIST[[stage]])){
+        DATA_LIST[[stage]] <<- read_loom(PATH_LIST[[stage]])
+    }
+
+    voxel_mat <- DATA_LIST[[stage]]$matrix
+
+    if (!is.null(structure_groups)){
         groups <- structure_groups
     } else {
-        meta <- filter(voxel_meta, voxel%in%rownames(expr_mat))
+        meta <- filter(voxel_meta, voxel%in%rownames(voxel_mat))
         groups <- meta[[annotation_level]]
     }
 
-    expr_mat[expr_mat < 1] <- 0
-    expr_mat <- log2(expr_mat + 1)
-    de_df <- de(expr_mat, groups)
+    voxel_mat[voxel_mat < 1] <- 0
+    voxel_mat <- log2(voxel_mat + 1)
+    de_df <- de(voxel_mat, groups)
     de_df <- arrange(de_df, group, desc(auc))
 
     return(de_df)
