48e76e214b873855ec6dde1602729968161a4d04
astair <jonas.simon.fleck@gmail.com>
2019-10-13 15:10:45 +0200

    Slice now class method for VoxelMap.

--- R/projection.R
+++ R/projection.R
@@ -1,32 +1,46 @@
-#' Map correlation matrix to brain slices and plot the result
+#' Slice VoxelMap and plot result
+#'
+#' @rdname slice
+#' @export slice
+#'
+slice <- function(object, ...){
+    UseMethod(generic = 'slice', object = object)
+}
+
+
+#' Slice VoxelMap and plot result
 #'
 #' @import dplyr
 #'
+#' @rdname slice
 #' @export
+#' @method slice VoxelMap
 #'
-project_to_slices <- function(corr_mat,
-                              cell_groups = NULL,
-                              slices = c(6, 11, 23, 28, 36),
-                              annotation_level = 'level_1',
-                              annotation_colors = many
+slice.VoxelMap <- function(
+    object,
+    slices = c(6, 11, 23, 28, 36),
+    groups = NULL,
+    annotation_level = 'level_1',
+    annotation_colors = many
 ){
-    cluster_cor <- aggregate_matrix(corr_mat, groups=cell_groups, fun=colMeans)
+    if (is.null(groups) & !is.null(object$cell_meta$groups)){
+        groups <- object$cell_meta$groups
+    }
+
+    cluster_cor <- aggregate_matrix(object$corr_mat, groups=groups, fun=colMeans)
 
     plot_df <- cluster_cor %>%
         as.matrix() %>%
         as_tibble(rownames='voxel') %>%
         tidyr::gather(cluster, corr, -voxel)
 
-    if (is.null(cell_groups)){
+    if (is.null(groups)){
         grouping_levels <- ' '
     } else{
-        grouping_levels <- levels(factor(cell_groups))
+        grouping_levels <- levels(factor(groups))
     }
 
-    utils::data(voxel_meta, envir = environment())
-    meta <- filter(voxel_meta, voxel%in%colnames(corr_mat)) %>%
-        tibble::column_to_rownames('voxel')
-
+    meta <- column_to_rownames(object$voxel_meta, 'voxel')
     plot_df$struct <- meta[plot_df$voxel, ][[annotation_level]]
     plot_df$x <- meta[plot_df$voxel, ]$x
     plot_df$y <- meta[plot_df$voxel, ]$y
@@ -39,7 +53,9 @@ project_to_slices <- function(corr_mat,
 
     slice_plot(plot_df, struct_colors = annotation_colors, newpage = T)
 
-    return(plot_df)
+    object$slices <- plot_df
+
+    return(object)
 }
 
 #' Map correlation matrix to brain grid and plot the result
