1a4ca17147a2583b0396d1856d44bb0b3ec4503f
astair <jonas.simon.fleck@gmail.com>
2019-10-14 13:55:27 +0200

    Plot annotation is VoxelMap class method now.

--- R/plots.R
+++ R/plots.R
@@ -1,3 +1,82 @@
+#' Plot brain structure annotation
+#'
+#' @rdname plot_annotation
+#' @export plot_annotation
+#'
+plot_annotation <- function(object = 'E13', ...){
+    UseMethod(generic = 'plot_annotation', object = object)
+}
+
+
+#' Plot brain structure annotation
+#'
+#' @import ggplot2
+#' @import dplyr
+#'
+#' @rdname plot_annotation
+#' @export
+#' @method plot_annotation default
+#'
+plot_annotation.default <- function(
+    object = 'E13',
+    annotation_level = 'custom_2',
+    annotation_colors = many,
+    show_coordinates = F,
+    show_legend = F,
+    alpha = 0.5
+){
+    utils::data(voxel_meta, envir = environment())
+    age <- stage_name(object)
+    m <- filter(voxel_meta, stage == age)
+    p <- ggplot(m, aes_string('x', 'y', fill = annotation_level)) +
+        geom_tile(alpha = alpha) +
+        scale_x_continuous(breaks = seq(0, 100, 2)) +
+        scale_y_continuous(breaks = seq(-100, 100, 2)) +
+        labs(title = age)
+    if (!show_coordinates){
+        p <- p + theme_void()
+    }
+    if (!show_legend){
+        p <- p + theme(legend.position = 'none')
+    }
+    return(p)
+}
+
+
+#' Plot brain structure annotation
+#'
+#' @import ggplot2
+#' @import dplyr
+#'
+#' @rdname plot_annotation
+#' @export
+#' @method plot_annotation VoxelMap
+#'
+plot_annotation.VoxelMap <- function(
+    object,
+    annotation_level = 'custom_2',
+    annotation_colors = many,
+    show_coordinates = F,
+    show_legend = F,
+    alpha = 0.5
+){
+    m <- object$voxel_meta
+    age <- unique(m$stage)
+    p <- ggplot(m, aes_string('x', 'y', fill = annotation_level)) +
+        geom_tile(alpha = alpha) +
+        scale_x_continuous(breaks = seq(0, 100, 2)) +
+        scale_y_continuous(breaks = seq(-100, 100, 2)) +
+        labs(title = age)
+    if (!show_coordinates){
+        p <- p + theme_void()
+    }
+    if (!show_legend){
+        p <- p + theme(legend.position = 'none')
+    }
+    return(p)
+}
+
+
 #' Plot correlation to brain slices and structure annotation
 #'
 #' @import ggplot2
@@ -76,35 +155,7 @@ brain_plot <- function(corr_df,
 }
 
 
-#' Plot brain annotation
-#'
-#' @import ggplot2
-#' @import dplyr
-#'
-#' @export
-#'
-#'
-brain_annotation_plot <- function(stage = 'E13pt5',
-                                  annotation_level = 'custom_2',
-                                  annotation_colors = many,
-                                  show_coordinates = F,
-                                  show_legend = F,
-                                  alpha = 0.5
-){
-    utils::data(voxel_meta, envir = environment())
-    m <- filter(voxel_meta, age==stage)
-    p <- ggplot(m, aes_string('x', 'y', fill=annotation_level)) +
-        geom_tile(alpha=alpha) +
-        scale_x_continuous(breaks = seq(0, 100, 2)) +
-        scale_y_continuous(breaks = seq(-100, 100, 2))
-    if (!show_coordinates){
-        p <- p + theme_void()
-    }
-    if (!show_legend){
-        p <- p + theme(legend.position = 'none')
-    }
-    return(p)
-}
+
 
 #' Plot gene expression across the mouse brain
 #'
@@ -165,64 +216,4 @@ xy_plot <- function(x, g){
         feature_theme
 }
 
-# single_feature_plot <- function(expr_mat, meta, gene,
-#                                 plot=tsne_plot,
-#                                 title=NULL,
-#                                 sort=T,
-#                                 scale=T){
-#     meta$cell <- as.character(meta$cell)
-#     expr_markers <- get_markers(expr_mat, gene, scale=scale) %>%
-#         mutate(cell=as.character(cell)) %>%
-#         right_join(meta)
-#     x <- expr_markers %>%
-#         filter(gene==gene)
-#     if (sort){
-#         x <- arrange(x, expr)
-#     }
-#     return(plot(x, gene))
-# }
-#
-# heatmap_plot <- function(expr_mat, groups, genes,
-#                          colors = blues_flat,
-#                          group_colors = many,
-#                          newpage = T
-# ){
-#
-#     top_markers <- unique(genes)
-#
-#     top_marker_expr <- get_markers(expr_mat, top_markers) %>%
-#         mutate(groups=factor(groups[match(.$cell, rownames(expr_mat))], levels=levels(factor(groups)))) %>%
-#         group_by(groups) %>%
-#         mutate(mean_expr=mean(expr)) %>%
-#         ungroup() %>%
-#         arrange(groups, mean_expr) %>%
-#         mutate(cell=factor(cell, levels=unique(cell)),
-#                gene=factor(gene, levels=top_markers))
-#
-#     p1 <- ggplot(top_marker_expr, aes(cell, gene, fill=expr)) +
-#         scale_fill_gradientn(colors=colors) +
-#         geom_raster() +
-#         scale_y_discrete(expand=expand_scale(add = c(0,0))) +
-#         theme(
-#             axis.text.x = element_blank(),
-#             axis.title.x = element_blank(),
-#             axis.title.y = element_blank(),
-#             axis.ticks.x = element_blank(),
-#             axis.ticks.y = element_blank(),
-#             panel.border = element_blank(),
-#             legend.position = 'none'
-#         )
-#     p2 <- ggplot(top_marker_expr, aes(cell, 1, fill=groups)) +
-#         geom_raster() +
-#         scale_fill_manual(values=group_colors) +
-#         scale_y_discrete(expand=expand_scale(add = c(0,0)), labels=function(breaks) {rep_along(breaks, " ")}) +
-#         theme(
-#             axis.text.x = element_blank(),
-#             axis.title.x = element_blank(),
-#             axis.title.y = element_blank(),
-#             axis.ticks.x = element_blank(),
-#             panel.border = element_blank(),
-#             legend.position="bottom"
-#         )
-#     ggarrange(p1, p2, padding=0, heights = c(40, 2), newpage = newpage)
-# }
+
