0898e90345f431a97814e9591c3e070713fc6cd4
Jonas Simon Fleck <jonas.fleck@bsse.ethz.ch>
2019-12-10 15:37:34 +0100

    Added variable_genes().

--- R/differential_expression.R
+++ R/differential_expression.R
@@ -43,3 +43,33 @@ de <- function(expr_mat, groups){
                     pval, padj, 'prcex_self'=pct_in, 'prcex_other'=pct_out)
     return(tibble::as_tibble(de_df))
 }
+
+#' Finds differentially expressed features for all structures on a given annotation level
+#'
+#' @export
+#'
+variable_genes <- function(
+    stage = 'E13pt5',
+    method = 'vst',
+    nfeatures = 2000
+){
+
+    if (!exists('DATA_LIST') | !exists('PATH_LIST')){
+        stop('Data has not been loaded. Please run load_aba_data() first.')
+    }
+
+    stage <- stage_name(stage)
+    if (is.null(DATA_LIST[[stage]])){
+        DATA_LIST[[stage]] <<- read_loom(PATH_LIST[[stage]])
+    }
+
+    voxel_mat <- DATA_LIST[[stage]]$matrix
+
+    var_feat <- Seurat::FindVariableFeatures(t(voxel_mat), verbose=F) %>%
+        dplyr::as_tibble(rownames='gene') %>%
+        dplyr::arrange(dplyr::desc(vst.variance.standardized)) %>%
+        dplyr::filter(dplyr::row_number() <= nfeatures) %>%
+        {colnames(.) <- stringr::str_replace_all(colnames(.), '\\.', '_'); .}
+
+    return(var_feat)
+}
