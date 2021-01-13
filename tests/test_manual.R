28c6a4437ec77bc732cb933f01b7df82e9402012
astair <jonas.simon.fleck@gmail.com>
2021-01-13 16:29:35 +0100

    Thinner color strip for mousebrain plots.

--- tests/test_manual.R
+++ tests/test_manual.R
@@ -53,7 +53,7 @@ plot_map(bsmap)
 plot_structure_similarity(bsmap)
 
 ### La Manno Mousebrain
-load_mousebrain_data('/Volumes/treutlein/PUBLIC_DATA/published/single_cell/2020_linnarson_dev_mouse_brain/')
+load_mousebrain_data('/Users/jfleck/data/2020_linnarson_dev_mouse_brain')
 pb_mbmap <- mousebrain_map(example_seurat, genes_use = top10, pseudobulk_groups = T, group_name = 'cluster')
 plot_structure_similarity(pb_mbmap, cluster=T)
 plot_structure_similarity(pb_mbmap, cluster=F)
