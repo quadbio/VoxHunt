c6d4b7bf18a872f1e12ac538082fe9395ce56490
joschif <jonas.simon.fleck@gmail.com>
2021-11-03 14:15:44 +0100

    Better printing for VoxelMaps + pseudobulk

--- R/voxel_map.R
+++ R/voxel_map.R
@@ -124,8 +124,9 @@ print.VoxelMap <- function(object){
     n_voxels <- dim(object$corr_mat)[2]
     n_genes <- length(object$genes)
     stage  <- unique(object$voxel_meta$stage)
+    entity <- ifelse(single_cell, 'cells', 'groups')
     cat(paste0(
-        'A VoxelMap object\n', n_cells, ' cells mapped to\n',
+        'A VoxelMap object\n', n_cells, ' ', entity, ' mapped to\n',
         n_voxels, ' voxels in the ', stage, ' mouse brain\nbased on ',
         n_genes, ' features\n'
     ))
